8b4877a5d406cc4d937155e1df8adb275bada223
Bartłomiej Krakowski <48633090+bartlomiej-krakowski@users.noreply.github.com>
2019-04-22 22:02:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,89 +3,182 @@ class Fullpage {
   constructor() {
 
     this.setVars();
-    
-    if(window.innerWidth >= this.breakpoint) {
+
+    if(this.setVars && window.innerWidth >= this.settings.breakpoint) {
       this.setEvents();
     }
 
   }
 
   setVars() {
-    this.body = document.querySelector('body');
     this.sections = document.querySelectorAll(`*[data-fullpage]`);
-    
-    this.index = 0;
-    this.scrolled = false;
-    
+    this.navigation = document.querySelector(`*[data-fullpage-nav]`)
+
+    this.settings = {
+      index: 0,
+      scrolled: false,
+      breakpoint: 550,
+      initialScroll: 0,
+    }
+
     this.arrows = {
       up: 38,
       down: 40,
+      home: 36,
+      end: 35,
     }
-    this.breakpoint = 550;
-    
+
   }
 
   setEvents() {
-    window.addEventListener('wheel', (e) => this.scrollHandler(e));
-    this.body.addEventListener('transitionend', (e) => this.enableScrolling(e));
-    document.addEventListener('keydown', (e) => this.pressHandler(e));
+    window.addEventListener('wheel', e => this.wheelHandler(e));
+    window.addEventListener('scroll', e => this.scrollHandler(e));
+    document.addEventListener('keydown', e => this.pressHandler(e));
+    window.addEventListener('load', () => this.navigationInit());
   }
 
 
-  scrollHandler(e) {
+  wheelHandler(e) {
+
+    e.preventDefault();
+
     if (e.deltaY < 0) {
       this.slideUp();
     }
-  
+
     else if (e.deltaY > 0) {
       this.slideDown();
     }
   }
-  
-   pressHandler(e) {
-    if (e.keyCode === this.arrows.up) {
-      this.slideUp();
-    } 
-    else if (e.keyCode === this.arrows.down) {
+
+  scrollHandler(e) {
+    const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (currentScroll > 0 && this.settings.initialScroll <= currentScroll){
       this.slideDown();
+    } else {
+      this.slideUp();
     }
-  }
-  
-   preventScrolling() {
-    this.scrolled = true;
+    this.settings.initialScroll = currentScroll;
   }
 
-   enableScrolling() {
-    this.scrolled = false;
+   pressHandler(e) {
+
+     Object.getOwnPropertyNames(this.arrows).forEach(key => {
+      let arrow = this.arrows[key];
+      if(e.keyCode === arrow) {
+        e.preventDefault();
+      }
+    });
+
+     switch (e.keyCode) {
+       case this.arrows.up:
+        this.slideUp();
+        break;
+       case this.arrows.down:
+        this.slideDown();
+        break;
+       case this.arrows.home:
+        this.scrollToTop();
+        break;
+       case this.arrows.end:
+        this.scrollToBottom();
+        break;
+     }
+
+  }
+
+  preventScrolling() {
+    this.settings.scrolled = true;
+
+    const test = this;
+
+    setTimeout(function() {
+      test.enableScrolling();
+    }, 500);
+  }
+
+  enableScrolling() {
+    this.settings.scrolled = false;
   }
 
   slideUp() {
-    if (this.index > 0 && !this.scrolled) {
-      this.index--;
-      let position = 0;
+    if (this.settings.index > 0 && !this.settings.scrolled) {
 
-      for (let i = 0; i < this.index; i++) {
-        position -= this.sections[i].offsetHeight;
+      for (let i = 0; i < this.settings.index; i++) {
+        this.scrollTo(i);
       }
-    
-      this.body.style.top = `${position}px`;
-      this.preventScrolling();
+
+      this.settings.index--;
+
     }
   }
-  
-   slideDown() {
-    if (this.index <= this.sections.length-2 && !this.scrolled) {
-      this.index++;
-      let position = 0;
-      
-      for (let i = 0; i < this.index; i++) {
-        position -= this.sections[i+1].offsetHeight;
+
+  slideDown() {
+    if (this.settings.index <= this.sections.length-2 && !this.settings.scrolled) {
+      this.settings.index++;
+
+      for (let i = 0; i < this.settings.index; i++) {
+        this.scrollTo(i+1);
       }
-    
-      this.body.style.top = `${position}px`;
-      this.preventScrolling();
+
     }
   }
+
+  scrollToTop() {
+    this.scrollTo(0);
+
+    this.settings.index = 0;
+  }
+
+  scrollToBottom() {
+    this.scrollTo(this.sections.length-1);
+    this.settings.index = this.sections.length-1;
+  }
+
+  scrollTo(target) {
+    this.sections[target].scrollIntoView({
+      block: 'start',
+      behavior: 'smooth',
+    });
+
+    this.preventScrolling();
+  }
+
+  navigationInit() {
+
+    for(let i = 0; i <= this.sections.length - 1; i++) {
+      const navItem = document.createElement('li');
+      const navItemCLickable = document.createElement('a');
+
+      navItemCLickable.setAttribute('data-fullpage-nav-item', i);
+      navItemCLickable.setAttribute('href', i);
+      navItem.appendChild(navItemCLickable);
+      this.navigation.appendChild(navItem);
+    }
+
+    document.querySelectorAll(`*[data-fullpage-nav-item]`).forEach(
+      (navItem) => navItem.addEventListener('click', this.navigationHandler)
+    )
+  }
+
+  navigationHandler(e) {
+    e.preventDefault();
+
+    const navTarget = e.currentTarget.getAttribute('data-fullpage-nav-item');
+
+    this.sections[navTarget].scrollIntoView({
+      block: 'start',
+      behavior: 'smooth',
+    });
+
+    this.scrollTo(navTarget);
+
+    this.settings.index = 0;
+    this.preventScrolling();
+
+  }
+
 }
 
 
